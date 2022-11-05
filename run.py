f966c2b94413cd200fe0244eb43b87ed4778b8ac
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-11-05 23:56:39 +0000

    simplying the code a bit, now removed data augmnetation

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ def main():
     parser.add_argument('--batch_test', default=128, type=int, help='batch size of unlabelled data for pseudo labelling')
     parser.add_argument('--epochs', default=150, type=int, help='total training steps')
     parser.add_argument('--path', '-p', default='/home/moucheng/projects_data/Kannada-MNIST', type=str, help='Path to the folder containing all of the data in csv')
-    parser.add_argument('--alpha', default=1.0, type=float, help='weight on pseudo labelling loss')
+    parser.add_argument('--alpha', default=0.01, type=float, help='weight on pseudo labelling loss')
     parser.add_argument('--ssl_start', default=40.0, type=float, help='epochs when we start to use ssl')
     parser.add_argument('--sup_aug', default=0, type=int, help='1 for using random aug on labelled data')
     parser.add_argument('--unsup_aug', default=1, type=int, help='1 for using random aug on unlabelled data')
